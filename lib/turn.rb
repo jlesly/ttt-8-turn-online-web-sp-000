817d86fcb407d96da71a68b2b10a0620a7ae62a8
Lesly Juarez <leslyj2@gmail.com>
2020-03-04 21:06:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -60,6 +60,8 @@ def turn(board)
    if valid_move?(board, index)
       move(board, index, current_player(board))
       turn(board)
+   else 
+     turn(board)
    end
    display_board(board)
 end
