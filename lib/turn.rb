71c82bba2c6bb5a2a45a740111822a4bfdfac159
Lesly Juarez <leslyj2@gmail.com>
2020-03-04 21:06:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -59,7 +59,7 @@ def turn(board)
    index = input_to_index(user_input)
    if valid_move?(board, index)
       move(board, index)
-      turn(board)
+      display_board(board)
    else 
      turn(board)
    end
